b0268ec8b79db81ee949e2327a8c9b31db05290d
仲野 亮平 <ryohei.nakano@hugmo.co.jp>
2019-06-26 14:49:37 +0900

    Initial Commit

---
+++ InputControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InputControl
+//
+//  Created by 仲野 亮平 on 2019/06/26.
+//  Copyright © 2019 仲野 亮平. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
